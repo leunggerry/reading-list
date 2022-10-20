13d0232b44261885f287707dab72c1c5525e5bc1
gigi.upNorth <leung.gerry@gmail.com>
2022-10-20 01:39:53 -0400

    [#3 Client] Frontend Api changes for queries and mutations

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/read-list', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/reading-list-collection", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
